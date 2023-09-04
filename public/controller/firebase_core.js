08200f38b9aa417ed17990f88f4a8ef978ae683d
Kamal <skurukula@uco.edu>
2023-09-04 15:34:00 -0500

    initial commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBRG_qDQcSyWum0vR3uR7M_wLoPSKLzr-Q",
+    authDomain: "skurukula-cmsc5373-webapp.firebaseapp.com",
+    projectId: "skurukula-cmsc5373-webapp",
+    storageBucket: "skurukula-cmsc5373-webapp.appspot.com",
+    messagingSenderId: "922295489526",
+    appId: "1:922295489526:web:b9c6d7e01042d72461add7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
